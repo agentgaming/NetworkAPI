46719ad01638d408c797e92d04a5f36fab7b842f
Dakota628 <dakota@dasigns.net>
2013-04-14 03:03:44 -0400

    Fixed Maven, Added Classes

---
+++ src/main/java/com/mike724/networkapi/NetworkRank.java
@@ -0,0 +1,13 @@
+package com.mike724.networkapi;
+
+/**
+ * User: Dakota
+ * Date: 4/14/13
+ * Time: 2:05 AM
+ */
+public enum NetworkRank {
+    USER,
+    MODERATOR,
+    ADMINISTRATOR,
+    OWNER
+}
